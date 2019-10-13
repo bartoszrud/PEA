13e74cfe7cf129fe007612a6a14b356d53d06dff
Bartosz R <bartoszrudnikowicz840@gmail.com>
2019-10-13 21:57:39 +0200

    Brute force using permutations from itertools

--- classes/TSP.py
+++ classes/TSP.py
@@ -1,9 +1,11 @@
 # import fileinput
+import itertools
 
 class TSP:
     def __init__(self):
         self.graph_matrix = list()
         self.route = []
+
 
     def read_from_file(self,file_name):
         file  = open(file_name)
@@ -23,3 +25,20 @@ class TSP:
 
         self.distance +=self.graph_matrix[route[size-1]][route[0]]
         return self.distance
+
+    def permutation_based_BF_alg(self):
+        default_perm = [i+1 for i in range(self.size-1)]
+        default_route = list(default_perm)
+        default_route.insert(0,0)
+        best_length = self.compute_distance(default_route)
+        best_perm = default_route
+
+        for i in itertools.permutations(default_perm):
+            i_list = list(i)
+            i_list.insert(0,0)
+            x = self.compute_distance(i_list)
+            if best_length > x:
+                best_length = x
+                best_perm = i_list
+
+        return best_perm , best_length
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from classes import TSP as t
 
 x = t.TSP()
-x.read_from_file('data/data10.txt')
+x.read_from_file('data/data12.txt')
 for i in x.graph_matrix:
     print(i)
 
 print(type(x.graph_matrix[1][1]))
-print(x.compute_distance([0,1,2,3,4,5,6,7,8,9]))
+# print(x.compute_distance([0,1,2,3,4,5,6,7,8,9]))
+best_p, best_l = x.permutation_based_BF_alg()
+print(best_p, best_l)
