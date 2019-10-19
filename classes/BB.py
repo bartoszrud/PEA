b32ba67bee4d775ba46ea375679d6c8d863c820b
Bartosz R <bartoszrudnikowicz840@gmail.com>
2019-10-19 20:55:36 +0200

    Added BB with lower bound(using MST) for symmetric TSP

--- classes/BB.py
+++ classes/BB.py
@@ -20,9 +20,107 @@ class BB:
         remaining = [i for i in range(self.graph_size)]
         self.BB_finding(remaining,current_perm, 0)
 
+    def MST_BB(self):
+        self.upper_bound = self.tsp.compute_distance([x for x in range(self.graph_size)])
+        self.best_distance = self.tsp.compute_distance([x for x in range(self.graph_size)])
+        self.best_route = [x for x in range(self.graph_size)]
+        current_perm = []
+        remaining = [i for i in range(self.graph_size)]
+        self.BB_with_MST_as_lower_bound(remaining,current_perm, 0)
+
+
+    def min_spanning_tree(self,V):
+        remaining_Vs = V[:]
+        Tree = []
+
+        def func(tup):
+            comp_element = self.graph_matrix[tup[0]][tup[1]]
+            return comp_element
+
+        if len(remaining_Vs)>1:
+            visited = { x:False for x in remaining_Vs}
+            v = remaining_Vs[0]
+            visited[v]= True
+            edges = []
+
+            for i in range(len(V)):
+                for u in remaining_Vs:
+                    if visited[u] == False and u != v:
+                        edges.append((v,u))
+
+
+
+                edges = sorted(edges, key = func, reverse=True)
+
+                leng = len(edges)
+
+                for j in range(leng):
+                    e = edges.pop()
+                    u = e[1]
+                    if visited[u] == False:
+                        Tree.append(e)
+                        visited[u] = True
+                        v = u
+                        break
+        return Tree
+
+
+    def MST_weight(self,Tree):
+        sum = 0
+
+
+        if len(Tree)>1:
+            for x,y in Tree:
+                sum = sum + self.graph_matrix[x][y]
+
+
+        return sum
+
+
+
+
+    def BB_with_MST_as_lower_bound(self, remaining_Vs, current_solution, V_idx):
+        # ONLY FOR UNDIRECTED GRAPH (PRIM ALG)
+        current_route = current_solution[:]
+        remaining_vertices = remaining_Vs[:]
+        # print(current_route)
+
+        if len(remaining_vertices)>0:
+            current_route.append(V_idx)
+            # print(remaining_vertices)
+            # print(V_idx)
+            remaining_vertices.remove(V_idx)
+
+            if len(remaining_vertices)>0:
+                for i in remaining_vertices:
+                    next_route = current_route[:]
+                    next_route.append(i)
+                    future_remaining = remaining_vertices[:]
+                    future_remaining.remove(i)
+
+                    # Lower bound as sum of  of the travelled distance so far
+                    # and the lower estimation of the remaining distance based on MST
+
+                    l_bound = ((self.tsp.compute_partial_length(next_route)) + (self.MST_weight(self.min_spanning_tree(future_remaining))))
+
+                    if (l_bound<self.upper_bound):
+                        # print(next_route)
+                        self.BB_with_MST_as_lower_bound(remaining_vertices,current_route,i)
+
+
+            else:
+                result = self.tsp.compute_distance(current_route)
+                if result<self.best_distance:
+                    self.best_route=current_route
+                    self.best_distance = result
+                    self.upper_bound = result
+
+
+
+
 
     def BB_finding(self, remaining_Vs, current_solution, V_idx):
-
+        # the simplest BB implementation without lower bound
 
         current_route = current_solution[:]
         remaining_vertices = remaining_Vs[:]
@@ -47,9 +145,9 @@ class BB:
                     self.best_route=current_route
                     self.best_distance = result
                     self.upper_bound = result
-        else:
-            result = self.tsp.compute_distance(current_route)
-            if result<self.best_distance:
-                self.best_route=current_route
-                self.best_distance = result
-                self.upper_bound = result
+        # else:
+        #     result = self.tsp.compute_distance(current_route)
+        #     if result<self.best_distance:
+        #         self.best_route=current_route
+        #         self.best_distance = result
+        #         self.upper_bound = result
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import time
 from classes import TSP as t
 from classes import BB as b
 
 
 x = t.TSP()
-x.read_from_file('data/data18.txt')
+x.read_from_file('data/data10.txt')
 for i in x.graph_matrix:
     print(i)
 
@@ -12,6 +13,7 @@ branch_and_bound = b.BB(x)
 print(type(x.graph_matrix[1][1]))
 
 
+# print(branch_and_bound.graph_matrix[10][13])
 # BF
 # x.DFS_based()
 # print(x.best_route)
@@ -22,7 +24,19 @@ print(type(x.graph_matrix[1][1]))
 
 
 #BB
+start1 = time.time()
 branch_and_bound.simple_BB()
+end1 = time.time()
+print("Time without lower bound= {}".format(end1-start1))
+print(branch_and_bound.best_route)
+print(branch_and_bound.best_distance)
+
+start = time.time()
+branch_and_bound.MST_BB()
+end = time.time()
+print("Time with lower bound based on MST= {}".format(end-start))
 print(branch_and_bound.best_route)
 print(branch_and_bound.best_distance)
 # [0, 3, 4, 2, 8, 7, 6, 9, 1, 5]
+
+# dla 16 [0, 2, 5, 14, 6, 12, 1, 8, 9, 15, 11, 7, 13, 4, 10, 3]
