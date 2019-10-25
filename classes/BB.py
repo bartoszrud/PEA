385016e8795a0c8c01bbcfa1285e6cb0b68e681a
Bartosz R <bartoszrudnikowicz840@gmail.com>
2019-10-25 15:48:53 +0200

    Added BB with lower bound for asymmetric TSP

--- classes/BB.py
+++ classes/BB.py
@@ -20,8 +20,18 @@ class BB:
         remaining = [i for i in range(self.graph_size)]
         self.BB_finding(remaining,current_perm, 0)
 
-    def MST_BB(self):
+    def BB_asymmetric(self):
+
         self.upper_bound = self.tsp.compute_distance([x for x in range(self.graph_size)])
+        self.best_distance = self.tsp.compute_distance([x for x in range(self.graph_size)])
+        self.best_route = [x for x in range(self.graph_size)]
+        current_perm = []
+        remaining = [i for i in range(self.graph_size)]
+        self.BB_with_two_min_edges_as_lower_bound(remaining,current_perm, 0)
+
+
+    def MST_BB_symmetric(self):
+        self.upper_bound = 2*(self.MST_weight(self.min_spanning_tree([x for x in range(self.graph_size)])))
         self.best_distance = self.tsp.compute_distance([x for x in range(self.graph_size)])
         self.best_route = [x for x in range(self.graph_size)]
         current_perm = []
@@ -49,7 +59,6 @@ class BB:
                         edges.append((v,u))
 
 
-
                 edges = sorted(edges, key = func, reverse=True)
 
                 leng = len(edges)
@@ -75,6 +84,20 @@ class BB:
 
 
         return sum
+
+    def two_min_edges_bound(self,V):
+        sum = 0
+
+        for v in V:
+            listv = self.graph_matrix[v][:]
+            listv = sorted(listv)
+            sum += listv[1] +listv [2]
+            #list[0] will be always route to node v itself (-1)
+        
+
+        sum = sum/2
+        return sum
+
 
 
 
@@ -109,11 +132,53 @@ class BB:
 
 
             else:
-                result = self.tsp.compute_distance(current_route)
+                result = self.tsp.compute_route(current_route)
                 if result<self.best_distance:
                     self.best_route=current_route
                     self.best_distance = result
                     self.upper_bound = result
+
+
+    def BB_with_two_min_edges_as_lower_bound(self, remaining_Vs, current_solution, V_idx):
+        # ONLY FOR UNDIRECTED GRAPH (PRIM ALG)
+        current_route = current_solution[:]
+        remaining_vertices = remaining_Vs[:]
+        # print(current_route)
+
+        if len(remaining_vertices)>0:
+            current_route.append(V_idx)
+            # print(remaining_vertices)
+            # print(V_idx)
+            remaining_vertices.remove(V_idx)
+
+            if len(remaining_vertices)>0:
+                for i in remaining_vertices:
+                    next_route = current_route[:]
+                    next_route.append(i)
+                    future_remaining = remaining_vertices[:]
+                    future_remaining.remove(i)
+
+                    # Lower bound as sum of  of the travelled distance so far
+                    # and the lower estimation of the remaining distance based on MST
+
+                    l_bound = (self.tsp.compute_partial_length(next_route) + self.two_min_edges_bound(future_remaining))
+
+                    if (l_bound<self.upper_bound):
+                        # print(next_route)
+                        self.BB_with_two_min_edges_as_lower_bound(remaining_vertices,current_route,i)
+
+
+            else:
+                result = self.tsp.compute_route(current_route)
+                if result<self.best_distance:
+                    self.best_route=current_route
+                    self.best_distance = result
+                    self.upper_bound = result
+
+
+
+
+
 
 
 
@@ -140,7 +205,7 @@ class BB:
                         self.BB_finding(remaining_vertices,current_route,i)
 
             else:
-                result = self.tsp.compute_distance(current_route)
+                result = self.tsp.compute_route(current_route)
                 if result<self.best_distance:
                     self.best_route=current_route
                     self.best_distance = result
